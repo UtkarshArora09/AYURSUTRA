827de546dd60cd0256c04115ad957cdfc2e3de43
Utkarsh Arora <utkarsh.arora09@gmail.com>
2026-08-10 16:52:20 +0530

    fix: disable CORS credentials to allow wildcard origins

--- rag-service/main.py
+++ rag-service/main.py
@@ -41,7 +41,7 @@ app = FastAPI(title="AyurSutra Advanced RAG Chatbot")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
